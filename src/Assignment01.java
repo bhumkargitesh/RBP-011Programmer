ef974fb58108e7d4939d745b07a06b14bd8763ec
Gitesh Bhumkar <giteshbhumkar@gmail.com>
2023-07-07 17:06:52 +0530

    Assignment01

---
+++ src/Assignment01.java
@@ -0,0 +1,129 @@
+public class Assignment01 {
+    public static void main(String[] args){
+
+        //    Example: - int - datatype , a - variable , 10 - values
+        int a = 10;
+        int b = 15;
+        int c = 20;
+        int d = 25;
+        int e = 30;
+        int f = 35;
+        int g = 40;
+        int h = 45;
+        int i = 50;
+        int j = 55;
+
+        System.out.println("values of different int variables = " +a + " "+
+                b +" " + c + " " + d + " " + e + " "+ f +" " +
+                g + " " + h + " " + i + " " + j);
+
+
+       //Example: - float  - datatype , num - variable , 1.5 - values
+        float num1 = 1.402f;
+        float num2 = 1.501f;
+        float num3 = 2.001f;
+        float num4 = 2.1025f;
+        float num5 = 3.100f;
+        float num6 = 3.200f;
+        float num7 = 1.800f;
+        float num8 = 2.704f;
+        float num9 = 3.401f;
+        float num10 = 2.501f;
+        System.out.println("values of different float variables = " + num1 + " "
+                +num2+ " " + num3 + " " + num4 + " " + num5 + " " + num6 + " " + num7 + " "
+                + num8 + " " + num9 + " "+ num10);
+
+        //Example: - double  - datatype , k - variable , 10.10 - values
+
+        double k = 10.10;
+        double l = 15.15;
+        double m = 20.20;
+        double n = 25.25;
+        double o = 30.3;
+        double p = 35.35;
+        double q = 40.45;
+        double r = 45.45;
+        double s = 50.55;
+        double t = 55.55;
+
+        System.out.println("values of different double  variables = " +k + " "+
+                l +" " + m + " " + n + " " + o + " "+ p +" " +
+                q + " " + r + " " + s + " " + t);
+
+
+        //    Example: - long - datatype , aa - variable , 10000- values
+        long aa = 10000;
+        long ba = 15000;
+        long ca = 20000;
+        long da = 25000;
+        long ea = 30000;
+        long fa = 35000;
+        long ga = 40000;
+        long ha = 45000;
+        long ia = 50000;
+        long ja = 55000;
+
+        System.out.println("values of different long variables = " +aa + " "+
+                ba +" " + ca + " " + da + " " + ea + " "+ fa +" " +
+                ga + " " + ha + " " + ia + " " + ja);
+
+
+        //    Example: - byte - datatype , a1 - variable , 100 - values
+        byte a1 = 100;
+        byte b1 = -100;
+        byte c1 = -20;
+        byte d1 = 25;
+        byte e1 = -30;
+        byte f1 = -35;
+        byte g1 = 40;
+        byte h1 = -45;
+        byte i1 = 50;
+        byte j1 = -55;
+
+        System.out.println("values of different byte variables = " +a1 + " "+
+                b1 +" " + c1 + " " + d1 + " " + e1 + " "+ f1 +" " +
+                g1 + " " + h1 + " " + i1 + " " + j1);
+
+        //    Example: - short - datatype , a2 - variable , 1000 - values
+        short a2 = 1000;
+        short b2 = 1005;
+        short c2 = 2000;
+        short d2 = 2005;
+        short e2 = 3000;
+        short f2 = 3005;
+        short g2 = 4000;
+        short h2 = 4005;
+        short i2 = 5000;
+        short j2 = 31000;
+
+        System.out.println("values of different short variables = " +a2 + " "+
+                b2 +" " + c2 + " " + d2 + " " + e2 + " "+ f2 +" " +
+                g2 + " " + h2 + " " + i2 + " " + j2);
+
+
+
+        //    Example: - char - datatype , initial1 - variable , 'A' - values
+
+        char initial1 = 'A';
+        char initial2 = 'B';
+        char initial3 = 'C';
+        char initial4 = 'D';
+        char initial5 = 'E';
+        char initial6 = 'F';
+        char initial7 = 'G';
+        char initial8 = 'H';
+        char initial9 = 'I';
+        char initial10 = 'J';
+
+        System.out.println("values of different Char variables = " +initial1 + " "+
+                initial2 +" " + initial3 + " " + initial4 + " " + initial5 + " "+ initial6 +" " +
+                initial7 + " " + initial8 + " " + initial9 + " " + initial10);
+
+
+
+
+
+
+
+    }
+}
---
+++ src/DemoDay1.java
@@ -0,0 +1,29 @@
+public class DemoDay1 {
+   public static void main(String[] args){
+
+       System.out.println("Hello");
+       //is the methods which returns output
+       System.out.println("Gitesh");
+       // printing my name
+       //there are 2 particular  words we must remember
+       //initialization - it is process of creating a variable and assigning a value
+       //declaration - it is a process of creating a variable
+
+       int a;
+       //created a variables  called a
+       int b = 10;
+       a = 15;
+       System.out.println("value of a=" + a);
+       //process of joining  2 different types
+
+       System.out.println("value of b=" + b);
+       // When both  are similar datatypes it prefer arithmetic  over concatenation
+
+       System.out.println(a + b);
+
+       System.out.println(a +" " + b);
+   }
+
+
+
+}
