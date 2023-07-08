b03997904eb3788d10a1b9a027b7b19da8bfab14
Gitesh Bhumkar <giteshbhumkar@gmail.com>
2023-07-08 22:27:10 +0530

    LeapYear

---
+++ src/LeapYear.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static  void  main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year");
+        int year = sc.nextInt();
+        if (isleapyear(year)){
+            System.out.println(year + "yes,is a leap year" );
+        }else{
+            System.out.println(year + "no it is not leap year" );
+        }
+    }
+    public static boolean isleapyear(int year){
+        if (year % 4 == 0){
+            if (year % 100 == 0){
+                if (year % 400 ==0 ){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }else {
+                return true;
+            }
+        }else {
+            return false;
+        }
+    }
+
+}
