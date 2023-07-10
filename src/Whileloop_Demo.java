939e30f8ae3dc0d5f6481a350392e492d6d102c6
Gitesh Bhumkar <giteshbhumkar@gmail.com>
2023-07-10 19:41:05 +0530

    Whileloop_Demo

---
+++ src/Whileloop_Demo.java
@@ -0,0 +1,11 @@
+public class Whileloop_Demo {
+    public static void main(String[] args) {
+        int num =50;
+        int i = 0, sum=0;
+        while(i<=50){
+            sum = sum + i;
+            i++;
+        }
+        System.out.println("the number of 1 to 50 is: "+ sum);
+    }
+}
