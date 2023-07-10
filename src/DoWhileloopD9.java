a284126cad7d4ef943bbb1c775c67a926039abf1
Gitesh Bhumkar <giteshbhumkar@gmail.com>
2023-07-10 23:02:34 +0530

    DoWhileloopD9

---
+++ src/DoWhileloopD9.java
@@ -0,0 +1,22 @@
+public class DoWhileloopD9 {
+    public static void main(String[] args) {
+
+
+    /*    int num = 10, i = 1, sum = 0;
+        do {
+            sum = sum + i;
+            i++;
+        }
+        while (i <= num);
+        System.out.println("the summation of 1 to 10 is: = " + sum);    */
+
+
+        int num = 2, i = 1, sum = 0;
+        do {
+            sum = sum + i;
+            i++;
+        }
+        while (i <= num);
+        System.out.println("the summation of 1 to 10 is: = " + sum);
+    }
+}
