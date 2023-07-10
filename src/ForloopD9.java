3dec7094d9a3edba5a24811de47644c403691dbd
Gitesh Bhumkar <giteshbhumkar@gmail.com>
2023-07-10 18:38:51 +0530

    ForloopD9

---
+++ src/ForloopD9.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ForloopD9 {
+    public static void main(String[] args) {
+
+        System.out.println("check even and odd  number");
+        for (int i = 0; i<5; i++ ){
+
+                System.out.println("even number:="+(2*i));
+
+                System.out.println("odd number:="+(2*i+1));
+            }
+        for (int i = 5; i>0; i-- ) {
+            System.out.println("even number:="+(2*i));
+
+            System.out.println("odd number:="+(2*i+1));
+
+        }
+
+//            System.out.println(2*i);
+//            System.out.println("even number");
+//            System.out.println(2*i+1);
+//            System.out.println("odd number");
+//        }
+//        for (int i = 0;i<5;i++){
+//            System.out.println(2*i+1);
+//            System.out.println("odd number");
+//        }
+    }
+}
