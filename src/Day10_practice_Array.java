e9720713331cd5bb9f30c9ab1b75338eed9fc2e7
Gitesh Bhumkar <giteshbhumkar@gmail.com>
2023-07-12 15:09:38 +0530

    Day10_practice_Array

---
+++ src/Day10_practice_Array.java
@@ -0,0 +1,173 @@
+//1) Java Program to print the elements of an array
+public class Day10_practice_Array {
+    public static void main(String[] args) {
+
+        // Ques 1) 1) Java Program to print the elements of an array.
+
+//        int[] arr = {-7, -5, 5, 10, 0, 3, 20, 25, 12};
+//
+//        System.out.print("Elements of given array are: ");
+//
+//
+//        for (int i = 0; i < arr.length; i++) {
+//
+//            System.out.print(arr[i] + " ");
+//        }
+//    }
+//}
+
+
+        //Ques 3) Java Program to print the largest element in an array
+
+//          double[] numArray = {32.4, -24.5, 30.10, 33.2, 55.10, 43.1, 5.7, -6.5};
+//          double largest = numArray[0];
+//
+//          for (double num : numArray) {
+//              if (largest < num)
+//                  largest = num;
+//          }
+//
+//          System.out.format("Largest element = %.2f", largest);
+//
+//      }
+//  }
+
+
+        // Ques 4) 4) Java Program to print the smallest element in an array
+
+//          int[] x
+//                  = new int[]{25, 110, 74, 75, 5};
+//
+//          System.out.println("Given array ");
+//
+//          for (int i = 0; i < x.length; i++) {
+//
+//              System.out.println(x[i]);
+//          }
+//
+//
+//          int minValue = x[0];
+//
+//
+//          for (int i = 0; i < x.length; i++) {
+//
+//
+//              if (x[i] < minValue)
+//
+//                  minValue = x[i];
+//          }
+//
+//          System.out.println(
+//                  "Smallest element present in given array: "
+//                          + minValue);
+//      }
+//  }
+//
+
+
+        //Ques 5 )Java Program to print the elements of an array present on an even position
+
+
+//        int[] E = new int[]{1, 2, 3, 4, 5, 6};
+//
+//
+//        for (int i = 1; i < E.length; i = i+2) {
+//
+//
+//            System.out.println(E[i]);
+//        }
+//    }
+//}
+
+
+        // Ques 6) Java Program to print the elements of an array in reverse order\
+
+//          int[] arr = new int[]{1, 2, 3, 4, 5};
+//          System.out.println("Original array: ");
+//          for (int i = 0; i < arr.length; i++) {
+//              System.out.print(arr[i] + " ");
+//          }
+//          System.out.println();
+//          System.out.println("Array in reverse order: ");
+//
+//          for (int i = arr.length - 1; i >= 0; i--) {
+//              System.out.print(arr[i] + " ");
+//          }
+//      }
+//  }
+
+
+        // Ques 7) Java Program to print the elements of an array present on odd position
+
+//          int[] odd = new int[]{1, 2, 3, 4, 5};
+//          System.out.println("Elements of given array present on odd position: ");
+//
+//          for (int i = 0; i < odd.length; i = i + 2) {
+//              System.out.println(odd[i]);
+//          }
+//      }
+//  }
+
+
+        // Ques 8) Java Program to print the duplicate elements of an array
+//
+//          int[] arr = new int[]{1, 2, 9, 4, 9, 1, 8, 8, 3};
+//          System.out.println("Duplicate elements in given array: ");
+//          for (int i = 0; i < arr.length; i++) {
+//              for (int j = i + 1; j < arr.length; j++) {
+//                  if (arr[i] == arr[j])
+//                      System.out.println(arr[j]);
+//              }
+//          }
+//      }
+//  }
+
+
+
+
+         // Ques 9) Java Program to sort the elements of an array in ascending order
+
+//          int[] arr = new int[]{5, 2, 8, 7, 1};
+//          int temp = 0;
+//
+//
+//          System.out.println("Elements of original array: ");
+//          for (int i = 0; i < arr.length; i++) {
+//              System.out.print(arr[i] + " ");
+//          }
+//
+//
+//          for (int i = 0; i < arr.length; i++) {
+//              for (int j = i + 1; j < arr.length; j++) {
+//                  if (arr[i] > arr[j]) {
+//                      temp = arr[i];
+//                      arr[i] = arr[j];
+//                      arr[j] = temp;
+//                  }
+//              }
+//          }
+//
+//          System.out.println();
+//
+//
+//          System.out.println("Elements of array sorted in ascending order: ");
+//          for (int i = 0; i < arr.length; i++) {
+//              System.out.print(arr[i] + " ");
+//          }
+//      }
+//  }
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
