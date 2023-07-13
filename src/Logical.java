f14b1c1e865122175f3f14eae702ebab77f1ddc0
Gitesh Bhumkar <giteshbhumkar@gmail.com>
2023-07-13 10:57:45 +0530

    main

---
+++ src/Logical.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Logical {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the age value from user");
+        int age = sc.nextInt();
+        //if (age >=18 && age <= 60){
+            if (age >=18 || age <= 60){
+            System.out.println("Eligible");
+        }
+        else{
+            System.out.println("Ineligible");
+        }
+
+    }
+
+
+}
